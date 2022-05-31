92c293a4cb4eddb50664347a36fe423b872e2a6c
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-05-31 14:05:59 +0300

    Подключение зависимостей

--- app/src/main/java/com/gmail/pavlovsv93/testtask/HitHubActivity.kt
+++ app/src/main/java/com/gmail/pavlovsv93/testtask/HitHubActivity.kt
@@ -3,9 +3,9 @@ package com.gmail.pavlovsv93.testtask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class HitHubActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+		setContentView(R.layout.activity_git_hub)
 	}
 }
